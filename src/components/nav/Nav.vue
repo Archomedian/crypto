b379da82cb101001a11e4fb62db362a7f55bf478
Archomedian <jay@LAPTOP-6NJR1S9D.localdomain>
2022-05-25 12:27:16 +0100

    👇🏽 added infinite scrolling, periodic api fetch and last update timer

---
+++ src/components/nav/Nav.vue
@@ -0,0 +1,71 @@
+<script lang="ts">
+import NavItem from './NavItem.vue'
+
+
+export default {
+	props: {
+	},
+	components: {
+		NavItem
+	},
+	data() {
+		return {}
+	},
+	created() {
+	},
+	methods: {
+	},
+}
+</script>
+
+<template>
+	<nav>
+		<NavItem to="/" text="Home">
+		</NavItem>
+		<NavItem to="/dashboard" text="Dashboard" />
+		<NavItem to="/controls" text="Controls" />
+		<NavItem to="/stats" text="Stats" />
+		<NavItem to="/prices" text="Prices" >
+			<template #dropdowns>
+				<RouterLink to="/pussy">pussy</RouterLink>
+			</template>
+		</NavItem>
+		<NavItem to="/about" text="About" />
+	</nav>
+
+	<RouterView />
+</template>
+
+
+<style>
+a,
+.pink {
+	text-decoration: none;
+	color: var(--paradise-pink);
+	transition: 0.4s;
+}
+
+nav a.router-link-exact-active {
+	color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+	background-color: transparent;
+}
+
+nav a:hover {
+	color: var(--color-text);
+}
+
+nav a {
+	display: inline-block;
+	padding: 0 1rem;
+	margin: 1rem 0;
+	border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+	border: 0;
+}
+
+</style>
---
+++ src/components/nav/NavItem.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+
+export default {
+	props: {
+		to: String,
+		text: String,
+	},
+	data() {
+		return {
+			hover: false
+		}
+	},
+	created() {
+	},
+	methods:{
+	}
+}
+</script>
+
+<template>
+	<div @mouseenter="this.hover = true" @mouseleave="this.hover = false">
+		<RouterLink :to="this.to" >
+			{{text}}
+		</RouterLink>
+	</div>
+	<slot name="dropdowns" id="dropdown-cnt" v-if="hover"></slot>
+</template>
+
+<style scoped>
+div {
+	display: inline-block;
+	/* padding: 0 1rem; */
+	/* margin: 1rem 0; */
+	border-left: 1px solid var(--color-border);
+}
+
+/* #dropdown-cnt {
+	display: block;
+} */
+</style>

a7fb480f4dabf2ae68af71adf47feac405edc449
Jay <jpontes1998@gmail.com>
2022-07-21 00:31:06 +0100

    fixed dependency on previous code that is now deleted, added example of .env file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/boiler-plate/TheWelcome.vue';
+// import TheWelcome from '@/components/boiler-plate/TheWelcome.vue';
 </script>
 
 <template>
